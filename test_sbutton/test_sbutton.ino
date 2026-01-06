7cfe0a2350eb7fd0cc780237a54324d1bcc5e17c
denis915959 <denis9159597861@yandex.ru>
2026-01-06 13:06:37 +0300

    6 января 2

--- test_sbutton/test_sbutton.ino
+++ test_sbutton/test_sbutton.ino
@@ -3,49 +3,33 @@
 #include <freertos/task.h>
 #include <freertos/queue.h>
 
-/*#define BTN1 12
-#define BTN2 13*/
-
-//QueueHandle_t q;
 class Buttons{
   private:
   QueueHandle_t q;
   const int on_off_pin = 25;
-  const int ok_pin = 27;
+  //const int ok_pin = 27;
   static void buttonReaderStatic(void* pv) {
     Buttons* instance = static_cast<Buttons*>(pv);
     instance->buttonReader();
   }
-  void buttonReader(/*void* pv*/) {
+  void buttonReader() {
     pinMode(on_off_pin, INPUT);
-    pinMode(ok_pin, INPUT);
+    int b1_prev = 0;
     
-    int b1_prev = 1, b2_prev = 1;
-    unsigned long t1 = 0, t2 = 0;
-    
-    while(1) {
+    while(1) { // этот цикл будет длиться до конца выполнения программы
         // Кнопка 1
         int b1 = digitalRead(on_off_pin);
-         if(b1 == 1) {
-            // Кнопка нажата
-            uint8_t data = 1;
+         if((b1 == 1)&&(b1_prev == 0)) { // если кнопка нажата
+            uint8_t data = 1; // 1, если кнопка 1 нажата
             xQueueSend(q, &data, 0);
         }
-        
-        // Кнопка 2
-        int b2 = digitalRead(ok_pin);
-         if(b2 == 1) {
-            // Кнопка нажата
-            uint8_t data = 2;
-            xQueueSend(q, &data, 0);
-        }
-        
-        vTaskDelay(50 / portTICK_PERIOD_MS); //10
+        b1_prev = b1;        
+        vTaskDelay(50 / portTICK_PERIOD_MS); //10 Приостанавливает выполнение текущей задачи на 50 миллисекунд. portTICK_PERIOD_MS - перевод "тиков" в мс
     }
   }
   public:
   Buttons(){
-    q = xQueueCreate(5, sizeof(uint8_t));
+    q = xQueueCreate(5, sizeof(uint8_t)); // 2 - приоритет (выше, чем у loop), это запуск потока
     if(q == NULL) {
       Serial.println("ERROR: Failed to create queue!");
       return;
@@ -53,16 +37,12 @@ class Buttons{
     xTaskCreate(buttonReaderStatic, "btn", 2048, this, 2, NULL);
   }
 
-  
-
-  bool on_click(){
+  bool on_off_click(){
     uint8_t data;
     if(xQueueReceive(q, &data, 0) == pdTRUE) {
       if(data==1){
         return(true);
-      } /*else{
-        return(false)
-      }*/
+      }
     } else{
       return(false);
     }
@@ -76,16 +56,6 @@ void setup() {
 }
 
 void loop() {
-   //Serial.println("k1");
-    //uint8_t data;
-    //Serial.println(digitalRead(25));
-    /*if(xQueueReceive(q, &data, 0) == pdTRUE) {
-      if(data==1){
-        Serial.println("1");
-      } else{
-        Serial.println("2");
-      }
-    }*/
     Serial.println("k1");
     if(buttons.on_click()==true){
       Serial.println("1");
